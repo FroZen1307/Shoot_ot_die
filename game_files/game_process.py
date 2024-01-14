bc5bfd88de5b041a7df71a98cdcdf816d446dfc3
FroZen1307 <ilya130707@mail.ru>
2024-01-14 21:23:14 +0400

    .

--- game_files/game_process.py
+++ game_files/game_process.py
@@ -4,12 +4,17 @@ import pygame
 class PlayerOne(pygame.sprite.Sprite):
 
     def __init__(self):
-        super().__init__(all_sprites)
+        super().__init__(player_sprites)
+        self.trigger = 0
         self.frames = []
+        self.cur_frame = 8
         self.cut_sheet()
-        self.image = pygame.transform.scale(self.frames[0], (130, 130))
-        self.position = (50, 260)
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
+        self.position = [350, 0]
         self.rect = self.rect.move(self.position)
+        self.last_dir = 'right'
+        self.allow = 1
+        self.mask = pygame.mask.from_surface(self.image)
 
     def cut_sheet(self):
         sheet = pygame.image.load('sprites\\Adventure_Character_Simple.png')
@@ -17,13 +22,71 @@ class PlayerOne(pygame.sprite.Sprite):
                                 sheet.get_height() // 20)
 
         for j in range(20):
-            for i in range(8):
-                frame_location = (self.rect.w * i, self.rect.h * j)
+            for w in range(8):
+                frame_location = (self.rect.w * w, self.rect.h * j)
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def running_left(self):
-        pass
+    def moving(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_d] and self.rect.x < 719:
+            self.last_dir = 'right'
+            if self.trigger == 0:
+                if self.cur_frame == 8:
+                    self.cur_frame = 16
+                elif self.cur_frame == 21:
+                    self.cur_frame = 16
+                else:
+                    self.cur_frame += 1
+                self.trigger = 4
+            else:
+                self.trigger -= 1
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
+            self.rect.x += 5
+        elif key[pygame.K_a] and self.rect.x > -40:
+            self.last_dir = 'left'
+            if self.trigger == 0:
+                if self.cur_frame == 8:
+                    self.cur_frame = 16
+                elif self.cur_frame == 21:
+                    self.cur_frame = 16
+                else:
+                    self.cur_frame += 1
+                self.trigger = 4
+            else:
+                self.trigger -= 1
+            self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)), True,
+                                               False)
+            self.rect.x -= 5
+        else:
+            if self.last_dir == 'right':
+                self.cur_frame = 8
+                self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
+            else:
+                self.cur_frame = 8
+                self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)),
+                                                   True,
+                                                   False)
+
+    def jump(self):
+        if pygame.key.get_pressed()[pygame.K_w] and self.allow == 1:
+            jump_power = 100
+            if jump_power > 0:
+                self.rect.y -= jump_power
+                jump_power -= 1
+            self.allow = 0
+
+    def gravity(self):
+        if self.rect.y < 10 and (50 < self.rect.x < 300 or 500 < self.rect.x < 750):
+            self.rect.y += 5
+        elif self.rect.y < 135 and 300 < self.rect.x < 500:
+            self.rect.y += 5
+        elif (self.rect.y < 260 and (self.rect.x < 50 or 750 < self.rect.x)) or (
+                60 < self.rect.y < 260 and (50 < self.rect.x < 300 or 500 < self.rect.x < 750)) or (
+                285 < self.rect.y < 260 and 300 < self.rect.x < 500):
+            self.rect.y += 5
+        else:
+            self.allow = 1
 
 
 class Land(pygame.sprite.Sprite):
@@ -33,6 +96,7 @@ class Land(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = width
         self.rect.y = height
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 class Field:
@@ -50,6 +114,9 @@ if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((800, 400))
     all_sprites = pygame.sprite.Group()
+    player_sprites = pygame.sprite.Group()
+    pygame.display.set_caption('Shoot or die')
+    land_mask = pygame.mask.from_surface(pygame.image.load('sprites\\platforms.png'))
 
     field = Field(screen)
     for i in range(0, 800, 50):
@@ -63,6 +130,7 @@ if __name__ == '__main__':
     player_one = PlayerOne()
 
     all_sprites.draw(field.screen)
+    player_sprites.draw(screen)
     pygame.display.flip()
     bg_changer = pygame.time.Clock()
     running = True
@@ -70,6 +138,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         if bg_changer.tick() % 2 == 0:
             if field.frame != 534:
                 field.frame += 1
@@ -77,6 +146,7 @@ if __name__ == '__main__':
                 field.bg = pygame.transform.scale(field.bg, (800, 400))
                 field.screen.blit(field.bg, (0, 0))
                 all_sprites.draw(field.screen)
+                player_sprites.draw(screen)
                 pygame.display.flip()
             else:
                 field.frame = 0
@@ -84,5 +154,11 @@ if __name__ == '__main__':
                 field.bg = pygame.transform.scale(field.bg, (800, 400))
                 field.screen.blit(field.bg, (0, 0))
                 all_sprites.draw(field.screen)
+                player_sprites.draw(screen)
                 pygame.display.flip()
+
+        player_one.gravity()
+        player_one.jump()
+        player_one.moving()
+        pygame.image.save(player_one.frames[0], 'frame_person.png')
     pygame.quit()
--- game_files/test.py
+++ game_files/test.py
@@ -1,36 +1,80 @@
 import pygame
+from pygame.locals import *
+
+# Инициализация Pygame
+pygame.init()
+
+# Параметры окна
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Цвета
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+# Класс для спрайта
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.velocity = pygame.math.Vector2(0, 0)
 
     def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
+        self.rect.x += self.velocity.x
+        self.rect.y += self.velocity.y
 
 
-if __name__ == '__main__':
-    running = True
-    pygame.init()
-    screen = pygame.display.set_mode((800, 400))
-    player_one = PlayerOne(100, 100)
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-    pygame.quit()
+# Создание спрайта и стен
+player = Player()
+player.rect.x = screen_width // 2
+player.rect.y = screen_height // 2
+
+left_wall = pygame.Rect(0, 0, 10, screen_height)
+right_wall = pygame.Rect(screen_width - 10, 0, 10, screen_height)
+
+# Группа спрайтов
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Игровой цикл
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                player.velocity.x = -5
+            if event.key == K_RIGHT:
+                player.velocity.x = 5
+        elif event.type == KEYUP:
+            if event.key == K_LEFT and player.velocity.x < 0:
+                player.velocity.x = 0
+            if event.key == K_RIGHT and player.velocity.x > 0:
+                player.velocity.x = 0
+
+    # Обновление положения спрайта
+    player.update()
+
+    # Проверка столкновений со стенами
+    if player.rect.colliderect(left_wall):
+        player.rect.left = left_wall.right
+        player.velocity.x = max(0, player.velocity.x)
+    if player.rect.colliderect(right_wall):
+        player.rect.right = right_wall.left
+        player.velocity.x = min(0, player.velocity.x)
+
+    # Отрисовка
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, left_wall)
+    pygame.draw.rect(screen, WHITE, right_wall)
+    all_sprites.draw(screen)
+
+    # Обновление экрана
+    pygame.display.flip()
+
+pygame.quit()
