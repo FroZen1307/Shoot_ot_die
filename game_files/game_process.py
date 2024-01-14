74b583af9ce6152104c5916f8770e29c38fb3a05
FroZen1307 <ilya130707@mail.ru>
2024-01-14 12:08:49 +0400

    .

--- game_files/game_process.py
+++ game_files/game_process.py
@@ -1,9 +1,34 @@
 import pygame
 
 
+class PlayerOne(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet()
+        self.image = pygame.transform.scale(self.frames[0], (130, 130))
+        self.position = (50, 260)
+        self.rect = self.rect.move(self.position)
+
+    def cut_sheet(self):
+        sheet = pygame.image.load('sprites\\Adventure_Character_Simple.png')
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // 8,
+                                sheet.get_height() // 20)
+
+        for j in range(20):
+            for i in range(8):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def running_left(self):
+        pass
+
+
 class Land(pygame.sprite.Sprite):
     def __init__(self, width, height):
-        super().__init__(lands_sprites)
+        super().__init__(all_sprites)
         self.image = pygame.image.load('sprites\\platforms.png')
         self.rect = self.image.get_rect()
         self.rect.x = width
@@ -24,13 +49,20 @@ class Field:
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((800, 400))
-    lands_sprites = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
 
     field = Field(screen)
     for i in range(0, 800, 50):
         Land(i, 350)
+    for k in range(50, 300, 50):
+        Land(k, 100)
+    for g in range(300, 500, 50):
+        Land(g, 225)
+    for m in range(500, 750, 50):
+        Land(m, 100)
+    player_one = PlayerOne()
 
-    lands_sprites.draw(field.screen)
+    all_sprites.draw(field.screen)
     pygame.display.flip()
     bg_changer = pygame.time.Clock()
     running = True
@@ -44,13 +76,13 @@ if __name__ == '__main__':
                 field.bg = pygame.image.load("BG_frames\\Frame_" + str(field.frame) + ".gif").convert_alpha()
                 field.bg = pygame.transform.scale(field.bg, (800, 400))
                 field.screen.blit(field.bg, (0, 0))
-                lands_sprites.draw(field.screen)
+                all_sprites.draw(field.screen)
                 pygame.display.flip()
             else:
                 field.frame = 0
                 field.bg = pygame.image.load("BG_frames\\Frame_" + str(field.frame) + ".gif").convert_alpha()
                 field.bg = pygame.transform.scale(field.bg, (800, 400))
                 field.screen.blit(field.bg, (0, 0))
-                lands_sprites.draw(field.screen)
+                all_sprites.draw(field.screen)
                 pygame.display.flip()
     pygame.quit()
---
+++ game_files/test.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+if __name__ == '__main__':
+    running = True
+    pygame.init()
+    screen = pygame.display.set_mode((800, 400))
+    player_one = PlayerOne(100, 100)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
