e71d49add23f2243a29192318968d29ffb83d0b7
FroZen1307 <ilya130707@mail.ru>
2024-01-18 18:15:18 +0400

    .

--- game_files/game_process.py
+++ game_files/game_process.py
@@ -1,6 +1,29 @@
 import pygame
 
 
+def drawing_platforms():
+    map = ['----------------',
+           '-              -',
+           '-              -',
+           '-----       ----',
+           '-              -',
+           '-     -----    -',
+           '-              -',
+           '----------------']
+
+    x = y = 0  # координаты
+    for row in map:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                pf = Land(x, y)
+                all_sprites.add(pf)
+                platforms.append(pf)
+
+            x += 50  # блоки платформы ставятся на ширине блоков
+        y += 50  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
+
+
 class PlayerOne(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -9,12 +32,18 @@ class PlayerOne(pygame.sprite.Sprite):
         self.frames = []
         self.cur_frame = 8
         self.cut_sheet()
-        self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
-        self.position = [350, 0]
-        self.rect = self.rect.move(self.position)
+        self.image = pygame.Surface((48, 48))
+        self.image.fill(pygame.Color(pygame.color.Color('black')))
+        #self.image = pygame.transform.scale(self.frames[self.cur_frame], (100, 100))
+        self.position = [50, 200]
+        self.rect = pygame.Rect(50, 200, 48, 48)
         self.last_dir = 'right'
-        self.allow = 1
-        self.mask = pygame.mask.from_surface(self.image)
+        self.yvel = 0
+        self.onGround = False
+        self.xvel = 0
+        self.MOVE_SPEED = 5
+        self.JUMP_POWER = 15
+        self.GRAVITY = 0.35
 
     def cut_sheet(self):
         sheet = pygame.image.load('sprites\\Adventure_Character_Simple.png')
@@ -27,24 +56,18 @@ class PlayerOne(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def moving(self):
+        self.onGround = False  # На земле ли я?
+
+    def update(self, platforms):
         key = pygame.key.get_pressed()
-        if key[pygame.K_d] and self.rect.x < 719:
-            self.last_dir = 'right'
-            if self.trigger == 0:
-                if self.cur_frame == 8:
-                    self.cur_frame = 16
-                elif self.cur_frame == 21:
-                    self.cur_frame = 16
-                else:
-                    self.cur_frame += 1
-                self.trigger = 4
-            else:
-                self.trigger -= 1
-            self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
-            self.rect.x += 5
-        elif key[pygame.K_a] and self.rect.x > -40:
+
+        if key[pygame.K_w]:
+            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+                self.yvel = -self.JUMP_POWER
+
+        if key[pygame.K_a]:
             self.last_dir = 'left'
+            self.xvel = self.MOVE_SPEED * -1  # Лево = x- n
             if self.trigger == 0:
                 if self.cur_frame == 8:
                     self.cur_frame = 16
@@ -55,49 +78,72 @@ class PlayerOne(pygame.sprite.Sprite):
                 self.trigger = 4
             else:
                 self.trigger -= 1
-            self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)), True,
-                                               False)
-            self.rect.x -= 5
-        else:
+            #self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)), True,
+                                              # False)
+
+        if key[pygame.K_d]:
+            self.last_dir = 'right'
+            self.xvel = self.MOVE_SPEED  # Право = x + n
+            if self.trigger == 0:
+                if self.cur_frame == 8:
+                    self.cur_frame = 16
+                elif self.cur_frame == 21:
+                    self.cur_frame = 16
+                else:
+                    self.cur_frame += 1
+                self.trigger = 4
+            else:
+                self.trigger -= 1
+            #self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
+
+        if not (key[pygame.K_d] or key[pygame.K_a]):
             if self.last_dir == 'right':
                 self.cur_frame = 8
-                self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
+                #self.image = pygame.transform.scale(self.frames[self.cur_frame], (130, 130))
             else:
                 self.cur_frame = 8
-                self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)),
-                                                   True,
-                                                   False)
+                #self.image = pygame.transform.flip(pygame.transform.scale(self.frames[self.cur_frame], (130, 130)),
+                                                   #True,
+                                                   #False)
+            self.xvel = 0
 
-    def jump(self):
-        if pygame.key.get_pressed()[pygame.K_w] and self.allow == 1:
-            jump_power = 17
-            while jump_power > 0:
-                for i in range(jump_power):
-                    self.rect.y -= 1
-                jump_power -= 1
-            self.allow = 0
+        if not self.onGround:
+            self.yvel += self.GRAVITY
 
-    def gravity(self):
-        if self.rect.y < 10 and (50 < self.rect.x < 300 or 500 < self.rect.x < 750):
-            self.rect.y += 5
-        elif self.rect.y < 135 and 300 < self.rect.x < 500:
-            self.rect.y += 5
-        elif (self.rect.y < 260 and (self.rect.x < 50 or 750 < self.rect.x)) or (
-                60 < self.rect.y < 260 and (50 < self.rect.x < 300 or 500 < self.rect.x < 750)) or (
-                285 < self.rect.y < 260 and 300 < self.rect.x < 500):
-            self.rect.y += 5
-        else:
-            self.allow = 1
+        self.onGround = False  # Мы не знаем, когда мы на земле((
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.collide(self.xvel, 0, platforms)
+
+    def collide(self, xvel, yvel, platforms):
+        for p in platforms:
+            if pygame.sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+
+                if xvel > 0:  # если движется вправо
+                    self.rect.right = p.rect.left  # то не движется вправо
+
+                if xvel < 0:  # если движется влево
+                    self.rect.left = p.rect.right  # то не движется влево
+
+                if yvel > 0:  # если падает вниз
+                    self.rect.bottom = p.rect.top  # то не падает вниз
+                    self.onGround = True  # и становится на что-то твердое
+                    self.yvel = 0  # и энергия падения пропадает
+
+                if yvel < 0:  # если движется вверхa
+                    self.rect.top = p.rect.bottom  # то не движется вверх
+                    self.yvel = 0  # и энергия прыжка пропадает
 
 
 class Land(pygame.sprite.Sprite):
-    def __init__(self, width, height):
-        super().__init__(all_sprites)
-        self.image = pygame.image.load('sprites\\platforms.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = width
-        self.rect.y = height
-        self.mask = pygame.mask.from_surface(self.image)
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(pygame.Color(pygame.color.Color('black')))
+        self.image = pygame.transform.scale(pygame.image.load('sprites\\platforms.png'), (50, 50))
+        self.rect = pygame.Rect(x, y, 50, 50)
 
 
 class Field:
@@ -117,17 +163,10 @@ if __name__ == '__main__':
     all_sprites = pygame.sprite.Group()
     player_sprites = pygame.sprite.Group()
     pygame.display.set_caption('Shoot or die')
-    land_mask = pygame.mask.from_surface(pygame.image.load('sprites\\platforms.png'))
+    platforms = []
+    drawing_platforms()
 
     field = Field(screen)
-    for i in range(0, 800, 50):
-        Land(i, 350)
-    for k in range(50, 300, 50):
-        Land(k, 100)
-    for g in range(300, 500, 50):
-        Land(g, 225)
-    for m in range(500, 750, 50):
-        Land(m, 100)
     player_one = PlayerOne()
 
     all_sprites.draw(field.screen)
@@ -158,7 +197,7 @@ if __name__ == '__main__':
                 player_sprites.draw(screen)
                 pygame.display.flip()
 
-        player_one.gravity()
-        player_one.jump()
-        player_one.moving()
+        player_one.update(all_sprites)
+        fps_timer = pygame.time.Clock()
+        # fps_timer.tick(60)
     pygame.quit()
